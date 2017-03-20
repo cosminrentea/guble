dea01ecf62021b60db36f54e9889d5ce4d1ed3fc
Cosmin Rentea <c.rentea@tarent.de>
2017-03-20 15:20:52 +0200

    all other metrics for Firebase

--- server/fcm/fcm_prometheus.go
+++ server/fcm/fcm_prometheus.go
@@ -5,14 +5,50 @@ import (
 )
 
 var (
-	pSentMessages = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "fcm_sent_messages",
+	pSent = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_sent",
 		Help: "Number of messages sent to FCM",
+	})
+
+	pSendErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_send_errors",
+		Help: "Number of FCM errors when trying to send",
+	})
+
+	pResponseErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_response_errors",
+		Help: "Number of FCM errors received as responses",
+	})
+
+	pResponseInternalErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_response_internal_errors",
+		Help: "Number of internal errors related to FCM responses",
+	})
+
+	pResponseNotRegisteredErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_response_not_registered_errors",
+		Help: "Number of errors related to not registered states in FCM connector",
+	})
+
+	pResponseReplacedCanonicalErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_response_replaced_canonical_errors",
+		Help: "Number of errors related to canonical IDs in FCM connector",
+	})
+
+	pResponseOtherErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fcm_response_other_errors",
+		Help: "Number of other errors related to responses in the FCM connector",
 	})
 )
 
 func init() {
 	prometheus.MustRegister(
-		pSentMessages,
+		pSent,
+		pSendErrors,
+		pResponseErrors,
+		pResponseInternalErrors,
+		pResponseNotRegisteredErrors,
+		pResponseReplacedCanonicalErrors,
+		pResponseOtherErrors,
 	)
 }
