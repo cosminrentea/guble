ead7429bec9db813b36669a4ef542e9b6a04d517
Cosmin Rentea <c.rentea@tarent.de>
2017-03-20 14:15:31 +0200

    Prometheus metrics for SMS

---
+++ server/sms/sms_prometheus.go
@@ -0,0 +1,34 @@
+package sms
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	pSent = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "sms_sent",
+		Help: "Number of sms sent to the SMS service",
+	})
+
+	pNexmoSendErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "sms_nexmo_send_errors",
+		Help: "Number of errors while trying to send sms to Nexmo",
+	})
+
+	pNexmoResponseErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "sms_nexmo_response_errors",
+		Help: "Number of errors received from Nexmo",
+	})
+
+	pNexmoResponseInternalErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "sms_nexmo_response_internal_errors",
+		Help: "Number of internal errors related to Nexmo responses",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(
+		pSent,
+		pNexmoSendErrors,
+		pNexmoResponseErrors,
+		pNexmoResponseInternalErrors,
+	)
+}
