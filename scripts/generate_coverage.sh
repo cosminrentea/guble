d98400a97bfad2011b3cdaa4912c2cb837340426
Cosmin Rentea <c.rentea@tarent.de>
2017-06-14 14:44:20 +0300

    improved the generate_coverage script

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -e
 # Requires local installation of: `github.com/wadey/gocovmerge`
 
-cd $GOPATH/src/github.com/cosminrentea/gobbler
+FULL_COVERAGE_OUT="full_cov.out"
+
+cd ${GOPATH}/src/github.com/cosminrentea/gobbler
 
 rm -rf ./cov
 mkdir cov
 
 i=0
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path './vendor/*' -not -path '*/_test.go' -type d);
 do
-    if ls ${dir}/*.go &> /dev/null; then
-        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=./cov/$i.out ./${dir}
-        i=$((i+1))
+    if ls ${dir}/*_test.go &> /dev/null; then
+        COVERAGE_OUT=`echo ${dir} | tr './' '-' `
+        echo "Generating test coverage for dir in file: ${dir} : ${COVERAGE_OUT}"
+        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=./cov/${COVERAGE_OUT}.out ./${dir}
     fi
 done
 
-gocovmerge ./cov/*.out > full_cov.out
+wait
+gocovmerge ./cov/*.out > ${FULL_COVERAGE_OUT}
 rm -rf ./cov
