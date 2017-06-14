ce4c67172ea7dbdc9c108ad46b7bffaf29ccee1d
Cosmin Rentea <c.rentea@tarent.de>
2017-06-14 16:16:05 +0300

    improving/parameterizing the generate_coverage.sh script and its invocation in travis.yml

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -e
 # Requires local installation of: `github.com/wadey/gocovmerge`
 
-FULL_COVERAGE_OUT="full_cov.out"
+FULL_COVERAGE_OUT=${1:-"full_coverage.out"}
+COV_DIR=${2:-"./cov"}
 
 cd ${GOPATH}/src/github.com/cosminrentea/gobbler
 
-rm -rf ./cov
-mkdir cov
+rm -rf ${COV_DIR}
+mkdir ${COV_DIR}
 
-i=0
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path './vendor/*' -not -path '*/_test.go' -type d);
 do
     if ls ${dir}/*_test.go &> /dev/null; then
         COVERAGE_OUT=`echo ${dir} | tr './' '-' `
         echo "Generating test coverage for dir in file: ${dir} : ${COVERAGE_OUT}"
-        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=./cov/${COVERAGE_OUT}.out ./${dir}
+        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=${COV_DIR}/${COVERAGE_OUT}.out ./${dir}
     fi
 done
 
 wait
-gocovmerge ./cov/*.out > ${FULL_COVERAGE_OUT}
-rm -rf ./cov
+
+gocovmerge ${COV_DIR}/*.out > ${FULL_COVERAGE_OUT}
+
+rm -rf ${COV_DIR}
